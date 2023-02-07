a0bb8a6928c31a49a33c873083e296d1a0055451
JiinArcade <jn_ll@naver.com>
2023-02-07 17:24:05 +0900

    💄 스타일링 중

--- JS.js
+++ JS.js
@@ -1,14 +1,3 @@
-const clock = document.getElementById("#clock"); 
-function getclock() {
-  const date = new Date;
-  const hours = String(date.getHours()).padStart(2, "0");
-  const min = String(date.getMinutes()).padStart(2, "0");
-  const sec = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${min}:${sec}`;
-}
-getclock();
-setInterval(getclock, 1000);
-
 let morningBtn = document.getElementById("morning");
 let afternoonBtn = document.getElementById("afternoon");
 let eveningBtn = document.getElementById("evening");
@@ -24,31 +13,39 @@ let font = document.querySelector(".font")
 morningBtn.addEventListener('click', function() {
   back.style.backgroundImage = 'url(./img/bg_morning.jpg)'
   hand.style.backgroundImage = 'url(./img/phone_morning.png)'
-  logow.style.display = "none"
-  logob.style.display = "block"
 });
 
 afternoonBtn.addEventListener('click', function() {
   back.style.backgroundImage = 'url(./img/bg_afternoon.jpg)'
   hand.style.backgroundImage = 'url(./img/phone_afternoon.png)'
-  logow.style.display = "none"
-  logob.style.display = "block"
 });
 
 eveningBtn.addEventListener('click', function() {
   back.style.backgroundImage = 'url(./img/bg_evening.jpg)';
   hand.style.backgroundImage = 'url(./img/phone_evening.png)';
-  logow.style.display = "none"
-  logob.style.display = "block"
 });
 
 nightBtn.addEventListener('click', function() {
   back.style.backgroundImage = 'url(./img/bg_night.jpg)'
   hand.style.backgroundImage = 'url(./img/phone_night.png)'
-  logob.style.display = "none"
-  logow.style.display = "block"
-  document.getElementsByClassName(".font").style.color = '#fff';
 });
+
+
+
+let time = document.querySelectorAll('.clock');
+
+setInterval(() => {
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  time[0].innerHTML = ('00' + hours).slice(-2);
+  time[2].innerHTML = ('00' + minutes).slice(-2);
+  time[4].innerHTML = ('00' + seconds).slice(-2);
+}, 1000);
+
+
 
 
 
