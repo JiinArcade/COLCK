eee084960acdb592db4454f29eb8ff06fcccab33
JiinArcade <jn_ll@naver.com>
2023-02-07 16:05:04 +0900

    💄 스타일링

---
+++ JS.js
@@ -0,0 +1,44 @@
+let morningBtn = document.getElementById("morning");
+let afternoonBtn = document.getElementById("afternoon");
+let eveningBtn = document.getElementById("evening");
+let nightBtn = document.getElementById("night");
+let back = document.getElementById("bgm");
+let hand = document.getElementById("clock-box");
+let logob = document.querySelector(".logob")
+let logow = document.querySelector(".logow")
+let font = document.querySelector(".font")
+
+
+morningBtn.addEventListener('click', function() {
+  back.style.backgroundImage = 'url(./img/bg_morning.jpg)'
+  hand.style.backgroundImage = 'url(./img/phone_morning.png)'
+  logow.style.display = "none"
+  logob.style.display = "block"
+});
+
+afternoonBtn.addEventListener('click', function() {
+  back.style.backgroundImage = 'url(./img/bg_afternoon.jpg)'
+  hand.style.backgroundImage = 'url(./img/phone_afternoon.png)'
+  logow.style.display = "none"
+  logob.style.display = "block"
+});
+
+eveningBtn.addEventListener('click', function() {
+  back.style.backgroundImage = 'url(./img/bg_evening.jpg)'
+  hand.style.backgroundImage = 'url(./img/phone_evening.png)'
+  logow.style.display = "none"
+  logob.style.display = "block"
+});
+
+nightBtn.addEventListener('click', function() {
+  back.style.backgroundImage = 'url(./img/bg_night.jpg)'
+  hand.style.backgroundImage = 'url(./img/phone_night.png)'
+  logob.style.display = "none"
+  logow.style.display = "block"
+  font.getElementsByClassName(".font").style.color = '#fff';
+});
+
+
+
+
+
